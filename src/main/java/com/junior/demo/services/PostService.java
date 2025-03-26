a5a1b4d1c59050d987fc6b613b4e0135aab324ce
Junior Paula <luke.junnior@icloud.com>
2025-03-26 19:10:21 -0300

    Add PostService and PostResource Get Post by id

---
+++ src/main/java/com/junior/demo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.junior.demo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.junior.demo.domain.Post;
+import com.junior.demo.repository.PostRepository;
+import com.junior.demo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+  @Autowired
+  private PostRepository PostRepository;
+
+  public Post findById(String id) {
+    Post Post = PostRepository.findById(id).orElse(null);
+    if (Post == null) {
+      throw new ObjectNotFoundException("Objeto não encontrado");
+    }
+    return Post;
+  }
+}
