9a50e75c5787a2c890567cf4ea2ff25ed178c742
Junior Paula <luke.junnior@icloud.com>
2025-03-26 13:14:22 -0300

    Implements dto partner

--- src/main/java/com/junior/demo/resources/UserResorce.java
+++ src/main/java/com/junior/demo/resources/UserResorce.java
@@ -1,6 +1,7 @@
 package com.junior.demo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.junior.demo.domain.User;
+import com.junior.demo.dto.UserDTO;
 import com.junior.demo.services.UserService;
 
 
@@ -20,8 +22,9 @@ public class UserResorce {
   private UserService service;
 
   @RequestMapping(method = RequestMethod.GET)
-  public ResponseEntity<List<User>> findAll() {
+  public ResponseEntity<List<UserDTO>> findAll() {
     List<User> users = service.findAll();
-    return ResponseEntity.ok().body(users);
+    List<UserDTO> listDto = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+    return ResponseEntity.ok().body(listDto);
   }
 }
