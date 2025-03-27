af8903f8f28ed531f060da6793db7a578454daa2
Junior Paula <luke.junnior@icloud.com>
2025-03-27 08:53:22 -0300

    Implements full query serach

--- src/main/java/com/junior/demo/resources/utils/URL.java
+++ src/main/java/com/junior/demo/resources/utils/URL.java
@@ -2,6 +2,9 @@ package com.junior.demo.resources.utils;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class URL {
 
@@ -10,6 +13,15 @@ public class URL {
       return URLDecoder.decode(text, "UTF-8");
     } catch (UnsupportedEncodingException e) {
       return "";
+    }
+  }
+
+  public static Date convertDate(String textDate, Date defaultValue) {
+    try {
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+      return sdf.parse(textDate);
+    } catch (ParseException e) {
+      return defaultValue;
     }
   }
 }
