9075be16a6c752646bc714a73d00521c2de7439a
Junior Paula <luke.junnior@icloud.com>
2025-03-27 08:07:22 -0300

    Add comments into posts

--- src/main/java/com/junior/demo/config/Instantiation.java
+++ src/main/java/com/junior/demo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.junior.demo.domain.Post;
 import com.junior.demo.domain.User;
 import com.junior.demo.dto.AuthorDTO;
+import com.junior.demo.dto.CommentDTO;
 import com.junior.demo.repository.PostRepository;
 import com.junior.demo.repository.UserRepository;
 
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner{
 
     Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(u1));
     Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(u1));
+
+    CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(u2));
+    CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(u3));
+    CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(u2));
+
+    p1.getComments().addAll(Arrays.asList(c1, c2));
+    p2.getComments().addAll(Arrays.asList(c3));
 
     postRepository.saveAll(Arrays.asList(p1, p2));
 
